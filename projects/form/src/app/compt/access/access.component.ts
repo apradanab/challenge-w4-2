3bc7ee4cd98bc212dc5e1c768e5f1cbff5a60876
apradanab <apradanab@gmail.com>
2024-03-20 08:13:52 +0100

    Add challenge

---
+++ projects/form/src/app/compt/access/access.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: 'app-access',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './access.component.html',
+  styleUrl: './access.component.css'
+})
+
+export default class AccessComponent {
+
+}
---
+++ projects/form/src/app/compt/confirmation/confirmation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: 'app-confirmation',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './confirmation.component.html',
+  styleUrl: './confirmation.component.css'
+})
+
+export class ConfirmationComponent {
+
+}
